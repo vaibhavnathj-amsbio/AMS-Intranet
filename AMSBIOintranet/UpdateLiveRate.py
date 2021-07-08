1514021699bccf85ed0ffde2a738ab56e4056d1a
vaibhavnathj-amsbio <vaibhavnathj@amsbio.com>
2021-07-08 12:48:45 +0100

    + live_rt updt script

---
+++ AMSBIOintranet/UpdateLiveRate.py
@@ -0,0 +1,34 @@
+import sqlite3
+import requests
+import json
+
+
+# Loading credentials from credentials.json
+f = open('API/credentials.json') 
+API_credentials = json.load(f)
+
+connection = sqlite3.connect("db.sqlite3")
+cursor = connection.cursor()
+
+live_rate_dict = { "USD": {"USD": 1, "JPY": 0, "GBP": 0, "EUR": 0, "CHF": 0},
+                    "JPY": {"USD": 0, "JPY": 1, "GBP": 0, "EUR": 0, "CHF": 0},
+                    "GBP": {"USD": 0, "JPY": 0, "GBP": 1, "EUR": 0, "CHF": 0},
+                    "EUR": {"USD": 0, "JPY": 0, "GBP": 0, "EUR": 1, "CHF": 0},
+                    "CHF": {"USD": 0, "JPY": 0, "GBP": 0, "EUR": 0, "CHF": 1}}
+
+
+url = "https://currency-exchange.p.rapidapi.com/exchange"
+headers = API_credentials["RapidAPI"]
+# counter = 1
+for key, value in live_rate_dict.items():
+        for inner_key, inner_value in value.items():
+            if key == inner_key:
+                pass
+            else:
+                querystring = {"from": key , "to": inner_key}
+                response = requests.request("GET", url, headers=headers, params=querystring)
+                rate = round(float(response.text),3)
+                # target_values = (counter, key, inner_key, rate)
+                cursor.execute("UPDATE homepage_livecurrencyrate SET live_rate = ? WHERE base_currency = ? and to_currency = ?", (rate,key,inner_key))
+                connection.commit()
+                # counter += 1
